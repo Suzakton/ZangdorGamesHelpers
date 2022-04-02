7f353582e9a6b71c7f24b86eddee05c4206210f0
Suzak <yannla34@gmail.com>
2022-04-02 16:26:49 +0200

    Update DontDestroyOnLoad.cs

--- Runtime/Components/DontDestroyOnLoad.cs
+++ Runtime/Components/DontDestroyOnLoad.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 namespace ZangdorGames.Helpers.Components
 {
+    /// <summary>
+    /// Add the DontDestroyOnLoad on the gameobject with this component.
+    /// </summary>
     public class DontDestroyOnLoad : MonoBehaviour 
     {
         private void Awake() 
